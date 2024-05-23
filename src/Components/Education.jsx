1c13422a73ec20aeacb8b45e1e3b86db44cc08ff
sushant-chavan <136911374+sushant707@users.noreply.github.com>
2024-05-23 16:19:50 +0000

    some lines of code added

---
+++ src/Components/Education.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import React from "react";
+
+const Education = () => {
+    return(
+        <section className="light" id="education">
+            <h2>Education</h2>
+        </section>
+    )
+}
+
+export default Education;
